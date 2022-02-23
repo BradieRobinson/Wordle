c654b151053b39cc7f34ccefdfbc34691b08a173
BradieRobinson <bradie@nettek.com.au>
2022-02-23 16:51:53 +1100

    updated to also include answers

--- reading_data/process_text_files.py
+++ reading_data/process_text_files.py
@@ -3,11 +3,26 @@ import pandas as pd
 
 def main():
     '''
-    Converts wordle-allowed-guesses into a useable dataframe.
-    Note for future work: frame.append will be deprecated in a later pandas verion.
+    File location for raw and filtered. Sends files through to "create_csv" for export.
     '''
+
+    raw_folder_loc = "../data/raw/"
+    filtered_folder_loc = "../data/filtered/"
+    raw_file_allowed_guesses = "wordle-allowed-guesses.txt"
+    raw_wordle_answers = "wordle-answers.txt"
+
+    create_csv(raw_folder_loc + raw_file_allowed_guesses, filtered_folder_loc + raw_file_allowed_guesses.split(".")[0])
+    create_csv(raw_folder_loc + raw_wordle_answers, filtered_folder_loc + raw_wordle_answers.split(".")[0])
+
+
+def create_csv(input_file, output_file):
+    '''
+    Converts wordle-allowed-guesses into a useable dataframe.
+    Note for future work: frame.append will be deprecated in a later pandas version.
+    '''
+
     df = pd.DataFrame(columns=["word", "0", "1", "2", "3", "4", "value"])
-    for word in [line.split()[0] for line in open("wordle-allowed-guesses.txt")]:
+    for word in [line.split()[0] for line in open(input_file)]:
         df = df.append({'word' : word,
                         '0' : word[0],
                         '1' : word[1],
@@ -16,7 +31,7 @@ def main():
                         '4' : word[4],
                         'value' : 0},
                         ignore_index=True)
-    df.to_csv("allowed-guesses-usable.csv", index=False)
+    df.to_csv(output_file + ".csv", index=False)
 
 if __name__ == "__main__":
     main()
--- statistical_data/letter_distribution.py
+++ statistical_data/letter_distribution.py
@@ -6,23 +6,41 @@ def main():
     '''
     Prints graph of distribution of letters in 1 - 5th position + overall distribution
     '''
-    df = pd.read_csv ('../reading_data/allowed-guesses-usable.csv')
+    folder_root = "../data/filtered/"
+
+    allowed_guesses_input = folder_root + "wordle-allowed-guesses.csv"
+    allowed_guesses_output = "letter_distribution/allowed_guesses/"
+
+    answers_input = folder_root + "wordle-answers.csv"
+    answers_output = "letter_distribution/answers/"
+
+    read_data_frame(allowed_guesses_input, allowed_guesses_output)
+    read_data_frame(answers_input, answers_output)
+
+    
+
+def read_data_frame(input_file, output_file):
+    '''
+    Read datafame and count occurance
+    '''
+    df = pd.read_csv(input_file)
     overall_lst = []
     for i in range(5):
         labels, counts = np.unique(df[str(i)].tolist(),return_counts=True)
         overall_lst.append(df[str(i)].tolist())
-        create_data_img(labels, counts, i)
+        create_data_img(labels, counts, i, output_file)
 
     labels, counts = np.unique(overall_lst,return_counts=True)    
-    create_data_img(labels, counts, "overall")
+    create_data_img(labels, counts, "overall", output_file)
 
-
-
-def create_data_img(labels, counts, img_name):
+def create_data_img(labels, counts, img_name, output_file):
+    '''
+    Create image of distribution
+    '''
     ticks = range(len(counts))
     plt.bar(ticks,counts, align='center')
     plt.xticks(ticks, labels)
-    plt.savefig("letter_distribution/allowed_gusses/" + str(img_name) + '.png')
+    plt.savefig(output_file + str(img_name) + '.png')
     plt.clf()
 
 
